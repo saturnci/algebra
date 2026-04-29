a990281dfa659f4d51fb3f66d76755115cd4782a
Jason Swett <jason@benfranklinlabs.com>
2026-04-29 19:32:39 -0400

    Replace consolidated filler_test.rb with two separate filler test files.

---
+++ test/models/filler_1_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Filler1Test < ActiveSupport::TestCase
+  test "filler 1" do
+    assert_equal 1, 1
+  end
+end
---
+++ test/models/filler_2_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Filler2Test < ActiveSupport::TestCase
+  test "filler 2" do
+    assert_equal 2, 2
+  end
+end
